309469448be027f04df97325e20c022c167881eb
Andrew Le Couteur Bisson <andy.lecouteurbisson@microtest.co.uk>
2020-09-21 13:01:48 +0100

    A Thing

---
+++ Allocation.cpp
@@ -0,0 +1,38 @@
+// Allocation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <memory>      // memory management
+
+class Thing
+{
+public:
+    Thing(std::string name, int size)
+    {
+        this->name = name;
+        this->size = size;
+        std::cout << "I made a thing!" << std::endl;
+    }
+
+    std::string name;
+    int size;
+
+    ~Thing()
+    {
+        std::cout << "It's a thing of the past" << std::endl;
+    }
+};
+
+void showThing(std::shared_ptr<Thing> t)
+{
+    std::cout << "showThing " << t->name << " " << t->size << std::endl;
+}
+
+int main()
+{
+     auto thing = std::make_shared<Thing>("fred", 5);
+
+     showThing(thing);
+
+}
